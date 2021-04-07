4ad1aec608a6ce1e91fc7c802c349310c22f6023
Georges <georges.dib@gmail.com>
2021-04-07 08:13:38 +0100

    Create mock.rs

---
+++ src/mock.rs
@@ -0,0 +1,142 @@
+// Copyright (C) 2021 Georges Dib.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+//! Mocks for synthetics module.
+
+#![cfg(test)]
+
+use super::*;
+use frame_support::{construct_runtime, pallet_prelude::GenesisBuild, parameter_types};
+use orml_traits::parameter_type_with_key;
+use primitives::TokenSymbol;
+use sp_core::H256;
+use sp_runtime::{testing::Header, traits::IdentityLookup};
+
+pub type BlockNumber = u64;
+pub type AccountId = u128;
+
+pub const ALICE: AccountId = 1;
+pub const BOB: AccountId = 2;
+pub const KUSD: CurrencyId = CurrencyId::Token(TokenSymbol::KUSD);
+pub const DOT: CurrencyId = CurrencyId::Token(TokenSymbol::DOT);
+
+mod synthetics {
+	pub use super::super::*;
+}
+
+parameter_types!(
+	pub const BlockHashCount: BlockNumber = 250;
+	pub const SyntheticsModuleId: ModuleId = ModuleId(*b"aca/synm");
+	pub const NativeCurrencyId: CurrencyId = KUSD;
+);
+
+impl frame_system::Config for Runtime {
+	type BaseCallFilter = ();
+	type Origin = Origin;
+	type Index = u64;
+	type BlockNumber = BlockNumber;
+	type Call = Call;
+	type Hash = H256;
+	type Hashing = sp_runtime::traits::BlakeTwo256;
+	type AccountId = AccountId;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = Event;
+	type BlockHashCount = BlockHashCount;
+	type BlockWeights = ();
+	type BlockLength = ();
+	type DbWeight = ();
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<Balance>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type SS58Prefix = ();
+}
+
+parameter_type_with_key! {
+	pub ExistentialDeposits: |_currency_id: CurrencyId| -> Balance {
+		Default::default()
+	};
+}
+
+impl orml_tokens::Config for Runtime {
+	type Event = Event;
+	type Balance = Balance;
+	type Amount = Amount;
+	type CurrencyId = CurrencyId;
+	type WeightInfo = ();
+	type ExistentialDeposits = ExistentialDeposits;
+	type OnDust = ();
+}
+
+impl synthetics::Config for Runtime {
+	type Event = Event;
+	type ModuleId = SyntheticsModuleId;
+	type Currency = Tokens;
+	type NativeCurrencyId = NativeCurrencyId;
+}
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Runtime>;
+type Block = frame_system::mocking::MockBlock<Runtime>;
+
+construct_runtime!(
+	pub enum Runtime where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic
+	{
+		System: frame_system::{Pallet, Call, Event<T>},
+		Synthetics: synthetics::{Pallet, Call, Event<T>, Config, Storage},
+		Tokens: orml_tokens::{Pallet, Storage, Event<T>, Config<T>},
+	}
+);
+
+pub struct ExtBuilder {
+	endowed_accounts: Vec<(AccountId, CurrencyId, Balance)>,
+}
+
+impl Default for ExtBuilder {
+	fn default() -> Self {
+		Self {
+			endowed_accounts: vec![
+				(ALICE, KUSD, 1_000_000_000_000_000_000u128),
+				(BOB, KUSD, 1_000_000_000_000_000_000u128),
+			],
+		}
+	}
+}
+
+impl ExtBuilder {
+	pub fn build(self) -> sp_io::TestExternalities {
+		let mut t = frame_system::GenesisConfig::default()
+			.build_storage::<Runtime>()
+			.unwrap();
+
+		orml_tokens::GenesisConfig::<Runtime> {
+			endowed_accounts: self.endowed_accounts,
+		}
+		.assimilate_storage(&mut t)
+		.unwrap();
+
+		synthetics::GenesisConfig::default()
+			.assimilate_storage::<Runtime>(&mut t)
+			.unwrap();
+
+		t.into()
+	}
+}
